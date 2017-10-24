e0130d439df27b51c4c3740aff546f19a9f01faa
Joseph Albrecht <joey@joseph-albrecht.me>
2017-10-23 20:26:18 -0400

    moved subst, subst2 and multisubst into substitute.org

--- multisubst.el
+++
@@ -1,9 +0,0 @@
-(multisubst 'best 'worst '(I am the worst worst worst))
-
-
-(defun multisubst (new old lat)
-  "Replace all instances of old in lat with new"
-  (cond
-   ((null lat) ())
-   ((equal old (car lat)) (cons new (multisubst new old (cdr lat))))
-   (t (cons (car lat) (multisubst new old (cdr lat))))))
